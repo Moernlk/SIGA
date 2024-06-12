c8c82688bfd198d87f650fd905136d9981069565
Moernlk <kelvinmorenooficial@gmail.com>
2024-06-12 02:50:29 -0300

    agora os botoes centrais da homepage se movem para qualquer local da tela com o arrastar do cursor.

--- script/calendario.js
+++ script/calendario.js
@@ -2,7 +2,7 @@
 function Calendario() {
   const agenda = document.getElementById("agenda");
   agenda.className = "agenda";
-  // Cleaning exixting content
+  // LIMPANDO TELA
   agenda.innerHTML = "";
 
   const semestre = document.createElement("div");
@@ -12,6 +12,7 @@ function Calendario() {
   let num = 0;
   let empty = 0;
 
+  // QUANTIDADE DE MESES NO CALENDARIO:
   for (m = 0; m < 12; m++) {
     const mes = document.createElement("div");
     mes.className = "mes";
@@ -24,8 +25,11 @@ function Calendario() {
     const daysSection = document.createElement("div");
     daysSection.className = "daysSection";
     mes.appendChild(daysSection);
-
+    
+    // LISTA DA SEMANA:
     const lWeek = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+    // LISTA DOS MESES:
     const mNome = [
       "JAN",
       "FEV",
@@ -42,20 +46,24 @@ function Calendario() {
     ];
     const dMes = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     const numDivs = 42;
+     
+    // ORGANIZAÇÃO DA SEMANA NO TOPO DOS MESES:
 
     for (w = 0; w < 7; w++) {
       const week = document.createElement("week");
       week.className = "week";
+
       weekSection.appendChild(week);
       week.innerText = lWeek[w];
     }
+       // ORGANIZAÇÃO DO TAMANHO DO MES NO CALENDARIO:
 
     for (let d = 0; d < numDivs; d++) {
       const day = document.createElement("div");
       day.className = "day";
       daysSection.appendChild(day);
 
-      if (num > 0 && num > dMes[m]) {
+      if (num > 0 && num > dMes[m] || num > 0 && num > dMes[m-12]) {
         day.style.background = "#3f5b69";
         day.innerText = "";
         num = num + 1;
@@ -64,8 +72,8 @@ function Calendario() {
         day.style.cursor = "initial";
         day.style.pointerEvents = "none";
       }
-
-      if (num > 0 && num <= dMes[m]) {
+        // INCERÇÃO DE DIAS NO MES
+      if (num > 0 && num <= dMes[m] || num > 0 &&  num <= dMes[m-12]) {
         day.innerText = num;
         num = num + 1;
         day.style.cursor = "pointer";
@@ -86,7 +94,7 @@ function Calendario() {
           day.style.transition = "background-color 0.3s, color 0.5s";
         });
       }
-
+        // DIAS DA SEMANA ANTERIORES AO INICIO DO MES:
       if (num <= 0) {
         day.style.background = "#3f5b69";
 
@@ -95,28 +103,42 @@ function Calendario() {
         day.style.pointerEvents = "none";
         if (m == 0) {
           day.innerText = dMes[m] + num;
+        }else if (m > 12) {
+          day.innerText = dMes[m - 1 - 12] + num;
+        
         } else {
           day.innerText = dMes[m - 1] + num;
         }
         num = num + 1;
       }
-
+        // DIAS DA SEMANA POSTERIORES AO FIM DO MES:
       if (day.innerText == "") {
         if (d > 34 && day.innerText == "") {
           day.style.backgroundColor = "transparent";
           day.style.border = "transparent";
-        } else {
+        } else if(m >= 12){
+          day.innerText = -dMes[m - 12] + num - 1;
+          day.style.color = "#00000050";
+        }else{
           day.innerText = -dMes[m] + num - 1;
           day.style.color = "#00000050";
         }
 
+        // CODIGO DO MES:
         empty = empty + 1;
         if (d == 41) {
           day.style.color = "white";
+          if (m >= 12) {
+            day.innerText = mNome[m-12];
+          }
+          else{
           day.innerText = mNome[m];
+          }
         }
       }
     }
+      // ALGORITMO DE REINÍCIO DA CONTAGEM DOS DIAS 
+
     num = (6 + (7 - empty)) * -1;
     if (num <= -6) {
       num = num + 7;
@@ -124,3 +146,5 @@ function Calendario() {
     empty = 0;
   }
 }
+
+Calendario();
